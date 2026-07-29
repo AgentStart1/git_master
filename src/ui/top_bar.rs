15edc26d58a07e2623a97356fd4e76dc2abc162c
AgentStart1 <295559726+AgentStart1@users.noreply.github.com>
2026-07-29 21:20:16 +0800

    Move Git work off the UI thread

--- src/ui/top_bar.rs
+++ src/ui/top_bar.rs
@@ -24,37 +24,37 @@ impl GitMasterApp {
             .cursor_pointer()
             .text_sm()
             .child("Open Directory")
-            .on_click(cx.listener(|_this, _event, _window, cx| {
+            .on_click(cx.listener(|this, _event, _window, cx| {
                 let receiver = cx.prompt_for_paths(PathPromptOptions {
                     files: false,
                     directories: true,
                     multiple: false,
                     prompt: Some("Select parent directory".into()),
                 });
-                cx.spawn(async move |entity: WeakEntity<GitMasterApp>, cx| {
-                    if let Ok(Ok(Some(paths))) = receiver.await
-                        && let Some(path) = paths.into_iter().next()
-                    {
-                        entity
-                            .update(cx, |this, cx| {
-                                this.begin_scan(path.clone());
-                                cx.notify();
-                            })
-                            .ok();
-                        let scan_path = path.clone();
-                        let repos = cx
-                            .background_executor()
-                            .spawn(async move { git_ops::scan_repos(&scan_path) })
-                            .await;
-                        entity
-                            .update(cx, |this, cx| {
-                                this.apply_scan(&path, repos);
-                                cx.notify();
-                            })
-                            .ok();
-                    }
-                })
-                .detach();
+                this.scan_task =
+                    Some(cx.spawn(async move |entity: WeakEntity<GitMasterApp>, cx| {
+                        if let Ok(Ok(Some(paths))) = receiver.await
+                            && let Some(path) = paths.into_iter().next()
+                        {
+                            entity
+                                .update(cx, |this, cx| {
+                                    this.begin_scan(path.clone());
+                                    cx.notify();
+                                })
+                                .ok();
+                            let scan_path = path.clone();
+                            let repos = cx
+                                .background_executor()
+                                .spawn(async move { git_ops::scan_repos(&scan_path) })
+                                .await;
+                            entity
+                                .update(cx, |this, cx| {
+                                    this.apply_scan(&path, repos);
+                                    cx.notify();
+                                })
+                                .ok();
+                        }
+                    }));
             }));
 
         div()
